258cd077e34f2cfd29ad09b0b48f558d817f2995
Drelf2018 <drelf2018@outlook.com>
2023-07-09 20:59:47 +0800

    feat: nicaiwoxinzengleshenme

---
+++ aml2py.go
@@ -0,0 +1,93 @@
+package aml2py
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+
+	aml "github.com/Drelf2018/go-api-markup-language"
+	"github.com/Drelf2020/utils"
+)
+
+// 转 value 为 python 格式
+func ValueToPython(typ, val string) string {
+	val = strings.Replace(val, ",constant", "", 1)
+	if typ == "str" {
+		if val == "none" {
+			return "\"\""
+		}
+		return "\"" + val + "\""
+	} else if typ == "bool" {
+		if val == "none" {
+			return "False"
+		}
+		return utils.Capitalize(val)
+	} else {
+		if val == "none" {
+			return "0"
+		}
+		return val
+	}
+}
+
+// 转为 Python 格式
+func SentenceToPython(sentence *aml.Sentence) (s string) {
+	s = sentence.Name
+	typ := strings.Replace(sentence.Type, "num", "int", 1)
+	val := sentence.Value
+	if typ != "" {
+		s += ": " + typ
+	}
+
+	if val != "" {
+		s += " = " + ValueToPython(typ, val)
+	}
+	return
+}
+
+func ToPythonFunc(format string, api *aml.Api) string {
+	r, o, all := []string{}, []string{}, []string{}
+	hint := api.Hint
+	params := []string{}
+	for _, sentence := range api.Params.Map {
+		params = append(params, fmt.Sprintf("%v (%v): %v", sentence.Name, sentence.Type, sentence.Hint))
+		if sentence.IsConstant() {
+			typ := strings.Replace(sentence.Type, "num", "int", 1)
+			all = append(all, sentence.Name+"="+ValueToPython(typ, sentence.Value))
+			continue
+		}
+		if sentence.IsRequired() {
+			r = append(r, SentenceToPython(sentence))
+		} else if sentence.IsOptional() {
+			o = append(o, SentenceToPython(sentence))
+		}
+		all = append(all, sentence.Name+"="+sentence.Name)
+	}
+	if len(api.Params.Map) != 0 {
+		hint += "\n\n    Args:\n        " + strings.Join(params, "\n\n        ")
+	}
+	args := append(r, o...)
+
+	format = strings.ReplaceAll(format, "demo", api.Function)
+	format = strings.ReplaceAll(format, "args", strings.Join(args, ", "))
+	format = strings.ReplaceAll(format, "hint", hint)
+	format = strings.ReplaceAll(format, "update(", "update("+strings.Join(all, ", "))
+	return format
+}
+
+//go:embed func.py
+var FUNC string
+
+//go:embed api.py
+var API string
+
+func ToPython(am aml.ApiManager, path string) (api, file string) {
+	include := FUNC[:strings.Index(FUNC, "# loop")]
+	include = strings.Replace(include, "path", path, 1)
+	function := utils.Slice(FUNC, "# loop", "# end", 0)
+	for name, api := range am.Output {
+		api.Function = name
+		include += ToPythonFunc(function, api)
+	}
+	return API, include
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/Drelf2018/aml2py
+
+go 1.20
+
+require (
+	github.com/Drelf2018/go-api-markup-language v0.0.0-20230709124104-b7a412a88e17
+	github.com/Drelf2020/utils v0.0.0-20230617054513-1c11ad3f45e9
+)
+
+require (
+	github.com/antonfisher/nested-logrus-formatter v1.3.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
